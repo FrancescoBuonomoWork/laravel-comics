2e7e9e023ef762b56a8e3e2a79afb85d07a277a8
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-12-11 16:07:36 +0100

    add data in web.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,6 +3,20 @@
 @section('content')
 
     <main>
-        main home page
+       <div class="container">
+        <div class="row">
+            <div class="col-2">
+                <div class="card">
+                    {{-- @foreach ($movies as $movie)
+
+                      <p>$movie['title'];</p>  
+                    @endforeach --}}
+                    {{-- <img src="" class="card-img-top" alt="..."> --}}
+
+                    {{ $data }}
+                </div>
+            </div>
+        </div>
+       </div>
     </main>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,16 +6,16 @@
                 <div class="logo">logo</div>
                 <nav class="">
                     <ul class="d-flex gap-3">
-                        <li><a href="#">Character</a></li>
-                        <li><a href="#">Comics</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">TV</a></li>
-                        <li><a href="#">Games</a></li>
-                        <li><a href="#">Collectibles</a></li>
-                        <li><a href="#">Videos</a></li>
-                        <li><a href="#">Fans</a></li>
-                        <li><a href="#">News</a></li>
-                        <li><a href="#">Shop</a></li>
+                        <li class="nav-list-item"><a href="#">Character</a></li>
+                        <li class="nav-list-item"><a href="#">Comics</a></li>
+                        <li class="nav-list-item"><a href="#">Movies</a></li>
+                        <li class="nav-list-item"><a href="#">TV</a></li>
+                        <li class="nav-list-item"><a href="#">Games</a></li>
+                        <li class="nav-list-item"><a href="#">Collectibles</a></li>
+                        <li class="nav-list-item"><a href="#">Videos</a></li>
+                        <li class="nav-list-item"><a href="#">Fans</a></li>
+                        <li class="nav-list-item"><a href="#">News</a></li>
+                        <li class="nav-list-item"><a href="#">Shop</a></li>
                     </ul>
                 </nav>
                 <div class="search-bar">
